64d2b167929902bcb6228dcdf358c4f20034a550
Marko4123 <marko.uzunov@gmail.com>
2021-12-05 17:27:45 +0200

    My Personal Portfolio

--- js/script.js
+++ js/script.js
@@ -3,4 +3,10 @@ let header = document.querySelector('header');
 
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
 }
